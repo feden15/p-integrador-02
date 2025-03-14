b784ec7b9da4a4b0df27ffbf16c643112d2632cc
Federico Nova <fedenova8@gmail.com>
2025-03-14 19:48:34 -0300

    Cargué Card.jsx, cree Card.scss y mostramos los productos

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,20 @@
+import './Card.scss'
 
-const Card = () => {
+const Card = ({ producto }) => {
   return (
-    <div>Card</div>
+    <div className="card">
+      <article className="card__article">
+        <div className="card__image-container">
+          <img className="card__image" src={producto.foto} alt={producto.nombre} />
+        </div>
+        <div className="card__content">
+          <h2 className="card__heading">{producto.nombre}</h2>
+          <div className="card__description">
+            <p>{producto.descripcion}</p>
+          </div>
+        </div>
+      </article>
+    </div>
   )
 }
 
