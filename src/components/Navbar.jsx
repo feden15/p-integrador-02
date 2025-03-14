0879875d82a47e657fbbe26f3dcf04bff1e85815
Federico Nova <fedenova8@gmail.com>
2025-03-14 18:42:25 -0300

    Creamos el header de la página
    Cargamos los siguientes archivos: Cabecera.jsx y .scss. Navbar.jsx y .scss. NavItem.jsx. SearchBar.jsx y .scss. Creamos y cargamos la constante menuItems.js (constants). Sacamos lo que 'sobra' de Inicio.jsx. Agregamos Cabacera y Footer a App,jsx, acompañando Rutas.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import './Navbar.scss'
+import menuItems from '../constants/menuItems'
+import NavItem from './NavItem'
+
+const Navbar = () => {
+    return (
+        <nav className="nav-bar">
+            <ul className="nav-bar__nav-list">
+                {
+                    menuItems.map((item) => (
+                        <NavItem item={item} key={item.id} />
+                    ))
+                }
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,28 @@
+import './SearchBar.scss'
+
+const SearchBar = () => {
+    return (
+        <div className="search-bar">
+            <div className="search-bar__logo-container">
+                <img src="imgs/logoLunarte.webp" alt="" />
+            </div>
+            <form action="#" className="search-bar__form-container">
+                <label htmlFor="busqueda" className="search-bar__form-label">Buscar</label>
+                <input type="search" id="busqueda" className="search-bar__form-search" />
+                <button type="submit" className="search-bar__form-submit">Buscar</button>
+            </form>
+            <div className="search-bar__carrito-container">
+                <img src="imgs/logoCarrito.webp" alt="Logo carrito de compras" />
+            </div>
+            <div className="menu-toogle">
+                <label htmlFor="menu" className="menu-toogle__label">
+                    <span className="menu-toogle__top-bread"></span>
+                    <span className="menu-toogle__meat"></span>
+                    <span className="menu-toogle__bottom-bread"></span>
+                </label>
+            </div>
+        </div>
+    )
+}
+
+export default SearchBar
