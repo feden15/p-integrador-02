5062a685a01f6a51bef6753e23838fbf74420c67
Federico Nova <fedenova8@gmail.com>
2025-03-15 16:20:08 -0300

    Cargamos CarritoContext.jsx

--- src/contexts/CarritoContext.jsx
+++ src/contexts/CarritoContext.jsx
@@ -1,8 +1,49 @@
+import { createContext } from "react";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 
-const CarritoContext = () => {
-  return (
-    <div>CarritoContext</div>
-  )
+const CarritoContext = createContext()
+
+const CarritoProvider = ({ children }) => {
+
+  const [agregarAlCarrito, eliminarDelCarrito, limpiarCarrito, carrito] = useLocalStorage('carrito', [])
+
+  function elProductoEstaEnElCarrito(producto) {
+
+    const nuevoArray = carrito.filter(prod => prod.id === producto.id)
+
+    return nuevoArray.length
+
+  }
+
+  function obtenerProductoDeCarrito(producto) {
+
+    return carrito.find(prod => prod.id === producto.id)
+
+  }
+
+  const agregarProductoAlCarritoContext = (producto) => {
+
+    if (!elProductoEstaEnElCarrito(producto)) {
+      producto.cantidad = 1
+      agregarAlCarrito(producto)
+    } else {
+      const productoDeCarrito = obtenerProductoDeCarrito(producto)
+      productoDeCarrito.cantidad++
+      window.localStorage.setItem('carrito', JSON.stringify(carrito))
+    }
+
+  }
+
+  const data = {
+    carrito,
+    elProductoEstaEnElCarrito,
+    obtenerProductoDeCarrito,
+    agregarProductoAlCarritoContext
+  }
+
+  return <CarritoContext.Provider value={data}>{children}</CarritoContext.Provider>
+
 }
 
+export { CarritoProvider }
 export default CarritoContext
