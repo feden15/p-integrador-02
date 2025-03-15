c1b0323c5813c7701029fc9ac3ce47ccd8067cda
Federico Nova <fedenova8@gmail.com>
2025-03-15 15:48:39 -0300

    Cargamos useLocalStorage.jsx

--- src/hooks/useLocalStorage.jsx
+++ src/hooks/useLocalStorage.jsx
@@ -1,8 +1,54 @@
+import { useState } from "react";
 
-const useLocalStorage = () => {
-  return (
-    <div>useLocalStorage</div>
-  )
+export const useLocalStorage = ( clave, valorInicial = [] ) => {
+
+    const getValoralmacenado = () => {
+
+        try {
+            const valorAlmacenado = window.localStorage.getItem(clave)
+            return valorAlmacenado ? JSON.stringify(valorAlmacenado) : valorInicial
+        } catch (error) {
+            console.error(`Error al obtener ${clave} del localStorage ${error}`);
+            return valorInicial
+        }
+
+    }
+
+    const [valorAlmacenado, setValorAlmacenado] = useState(getValorAlmacenado())
+
+    const guardarValor = (valorNuevo) => {
+
+        try {
+            const nuevoValorAlmacenado = [...valorAlmacenado, valorNuevo]
+            setValorAlmacenado(nuevoValorAlmacenado)
+            window.localStorage.setItem(clave, JSON.stringify(nuevoValorAlmacenado))
+        } catch (error) {
+            console.error(`Error al guardar ${clave} del localStorage ${error}`);
+        }
+
+    }
+
+    const eliminarValor = (id) => {
+
+        try {
+            const nuevoValorAlmacenado = [...valorAlmacenado]
+            const indice = nuevoValorAlmacenado.findIndex(item => item.id === id)
+            nuevoValorAlmacenado.splice(indice, 1)
+            setValorAlmacenado(nuevoValorAlmacenado)
+            window.localStorage.setItem(clave, JSON.stringify(nuevoValorAlmacenado))
+        } catch (error) {
+            console.error(`Error al eliminar ${clave} del localStorage con ${id} del producto ${error}`);
+        }
+
+    }
+
+    const limpiarValores = () => {
+
+        window.localStorage.clear()
+        window.localStorage.setItem(clave, JSON.stringifya(valorInicial))
+        setValorAlmacenado(valorInicial)
+
+    }
+
+    return [guardarValor, eliminarValor, limpiarValores, valorAlmacenado]
 }
-
-export default useLocalStorage
