ade86692e0d80193a563ea8b12ef8dd9915235f2
Federico Nova <fedenova8@gmail.com>
2025-03-31 16:08:56 -0300

    Creamos handleChange, para cuando el user cargue el form

--- src/components/components-alta/Formulario.jsx
+++ src/components/components-alta/Formulario.jsx
@@ -23,19 +23,25 @@ const Formulario = () => {
 
     const [form, setForm] = useState(formInicial)
 
-    useEffect(() => { 
-      productoAEditar ? setForm(productoAEditar) : setForm(formInicial)
+    useEffect(() => {
+        productoAEditar ? setForm(productoAEditar) : setForm(formInicial)
     }, [productoAEditar])
-    
+
     const handleSubmit = (e) => {
         e.preventDefault()
 
-        if (form.id  === null) {
+        if (form.id === null) {
             crearProductoContext(form)
         } else {
             actualizarProductocontext(form)
         }
     }
+
+    const handleChange = (e) => {
+        const { type, name, checked, value } = e.target
+        setForm({ ...form, [name]: type === 'checkbox' ? checked : value })
+    }
+
 
     return (
         <>
@@ -49,7 +55,8 @@ const Formulario = () => {
                         type="text"
                         id="lbl-nombre"
                         name="nombre"
-                    />
+                        value={form.nombre}
+                        onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="lbl-precio">Precio</label>
@@ -57,7 +64,8 @@ const Formulario = () => {
                         type="text"
                         id="lbl-precio"
                         name="precio"
-                    />
+                        value={form.precio}
+                        onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="lbl-stock">Stock</label>
@@ -65,7 +73,8 @@ const Formulario = () => {
                         type="text"
                         id="lbl-stock"
                         name="stock"
-                    />
+                        value={form.stock}
+                        onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="lbl-marca">Marca</label>
@@ -73,7 +82,8 @@ const Formulario = () => {
                         type="text"
                         id="lbl-marca"
                         name="marca"
-                    />
+                        value={form.marca}
+                        onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="lbl-categoria">Categoría</label>
@@ -81,7 +91,8 @@ const Formulario = () => {
                         type="text"
                         id="lbl-categoria"
                         name="categoria"
-                    />
+                        value={form.categoria}
+                        onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="lbl-detalles">Detalles</label>
@@ -89,7 +100,8 @@ const Formulario = () => {
                         type="text"
                         id="lbl-detalles"
                         name="detalles"
-                    />
+                        value={form.detalles}
+                        onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="lbl-foto">Foto</label>
@@ -97,15 +109,17 @@ const Formulario = () => {
                         type="text"
                         id="lbl-foto"
                         name="foto"
-                    />
+                        value={form.foto}
+                        onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="lbl-envio">Envío</label>
                     <input
-                        type="text"
+                        type="checkbox"
                         id="lbl-envio"
                         name="envio"
-                    />
+                        checked={form.envio}
+                        onChange={handleChange} />
                 </div>
 
                 <button type="submit">Guardar : Editar</button>
